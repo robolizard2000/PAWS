51c819d3326b2d5b54b7c34c89b77132d580a398
robolizard2000 <robolizard2000@gmail.com>
2026-02-28 22:17:04 +0000

    Stop tracking the compiled output

--- Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/randomNumberGen/obj/local/arm64-v8a/objs-debug/webp/src/enc/config_enc.o.d
+++
@@ -1,8 +0,0 @@
-/home/lizard/Documents/Programming/Code/PAWS/Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/enc/config_enc.o: \
-  /home/lizard/Documents/Programming/Code/PAWS/Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/enc/config_enc.c \
-  /home/lizard/Documents/Programming/Code/PAWS/Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h \
-  /home/lizard/Documents/Programming/Code/PAWS/Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h
-
-/home/lizard/Documents/Programming/Code/PAWS/Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h:
-
-/home/lizard/Documents/Programming/Code/PAWS/Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h:
--- Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/randomNumberGen/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/lizard/Documents/Programming/Code/PAWS/Mobile-App/test-app-blE/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/lizard/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.c \
-  /home/lizard/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h
-
-/home/lizard/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h:
